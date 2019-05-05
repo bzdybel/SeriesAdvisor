3bb6156cd5646f0d95a1a8796041cbd8750164da
bartlomiej <bartlomiej.zdybel@gmail.com>
2019-05-05 18:19:08 +0200

    add responsive

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,63 +1,69 @@
-import React from 'react'
-import NoAccountInformation from './NoAccountInformation'
+import React from 'react';
+import NoAccountInformation from './NoAccountInformation';
 
 class LoginForm extends React.Component {
-	state = {
-		email: undefined,
-		password: undefined,
-	}
+    state = {
+        email: undefined,
+        password: undefined,
+    };
 
-	handleEmailChange = e => {
-		this.setState({
-			email: e.target.value,
-		})
-	}
-	handlePasswordChange = e => {
-		this.setState({
-			password: e.target.value,
-		})
-	}
-	handleLoginFormSubmit = e => {
-		e.preventDefault()
-		console.log(this.state.email)
-		console.log(this.state.password)
-	}
-	render() {
-		return (
-			<form className="login-container__form" onSubmit={this.handleLoginFormSubmit}>
-				<h1 className="login-container__form--title">Zaloguj się</h1>
-				<label htmlFor="email" className="login-container__form-label">
-					Email
-				</label>
-				<input
-					onChange={this.handleEmailChange}
-					value={this.state.email}
-					id="email"
-					name="email"
-					className="login-container__form--input"
-					type="text"
-					placeholder="james.cook@gmail.com"
-					required
-				/>
-				<label htmlFor="password" className="login-container__form-label">
-					Hasło
-				</label>
+    handleEmailChange = e => {
+        this.setState({
+            email: e.target.value,
+        });
+    };
+    handlePasswordChange = e => {
+        this.setState({
+            password: e.target.value,
+        });
+    };
+    handleLoginFormSubmit = e => {
+        e.preventDefault();
+        console.log(this.state.email);
+        console.log(this.state.password);
+    };
+    render() {
+        return (
+            <form
+                className="login-container__form"
+                onSubmit={this.handleLoginFormSubmit}
+            >
+                <h1 className="login-container__form--title">Zaloguj się</h1>
+                <label htmlFor="email" className="login-container__form-label">
+                    Email
+                </label>
+                <input
+                    onChange={this.handleEmailChange}
+                    value={this.state.email}
+                    id="email"
+                    name="email"
+                    className="login-container__form--input"
+                    type="text"
+                    placeholder="james.cook@gmail.com"
+                    required
+                />
+                <label
+                    htmlFor="password"
+                    className="login-container__form-label"
+                >
+                    Hasło
+                </label>
 
-				<input
-					onChange={this.handlePasswordChange}
-					value={this.state.password}
-					id="password"
-					className="login-container__form--input"
-					type="password"
-					placeholder="Password..."
-					required
-				/>
-				<button type="submit" className="login-container__form--button">
-					Zaloguj się
-				</button>
-				<NoAccountInformation />
-			</form>
-		)
-	}
+                <input
+                    onChange={this.handlePasswordChange}
+                    value={this.state.password}
+                    id="password"
+                    className="login-container__form--input"
+                    type="password"
+                    placeholder="Password..."
+                    required
+                />
+                <button type="submit" className="login-container__form--button">
+                    Zaloguj się
+                </button>
+                <NoAccountInformation />
+            </form>
+        );
+    }
 }
-export default LoginForm
+export default LoginForm;
