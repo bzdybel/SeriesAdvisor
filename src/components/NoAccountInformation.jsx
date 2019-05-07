c919996d3d78fc5f4c48f338be77a8dfec22aa67
bartlomiej <bartlomiej.zdybel@gmail.com>
2019-05-07 18:24:49 +0200

    add prettier to dev dependencies

--- src/components/NoAccountInformation.jsx
+++ src/components/NoAccountInformation.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NoAccountInformation = () => (
     <div className="login-section__no-account">
-        <small class="login-section__no-account-description">
+        <small className="login-section__no-account-description">
             Nie masz konta w serwisie Series Advisor?
         </small>
         <Link to="register" className="login-section__registration-link">
