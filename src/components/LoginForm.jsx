c26d6671960748e4ecd062e2345d573f32431082
bartlomiej <bartlomiej.zdybel@gmail.com>
2019-05-14 17:22:58 +0200

    change classes names

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -17,7 +17,7 @@ class LoginForm extends React.Component {
             password: e.target.value,
         });
     };
-    handleLoginFormSubmit = e => {
+    handleloginFormSubmit = e => {
         e.preventDefault();
         console.log(this.state.email);
         console.log(this.state.password);
@@ -25,11 +25,14 @@ class LoginForm extends React.Component {
     render() {
         return (
             <form
-                className="login-section__form"
-                onSubmit={this.handleLoginFormSubmit}
+                className="login-register-section__form"
+                onSubmit={this.handleloginFormSubmit}
             >
-                <h1 className="login-section__title">Zaloguj się</h1>
-                <label htmlFor="email" className="login-section__label">
+                <h1 className="login-register-section__title">Zaloguj się</h1>
+                <label
+                    htmlFor="email"
+                    className="login-register-section__label"
+                >
                     Email
                 </label>
                 <input
@@ -37,12 +40,15 @@ class LoginForm extends React.Component {
                     value={this.state.email}
                     id="email"
                     name="email"
-                    className="login-section__input"
+                    className="login-register-section__input"
                     type="email"
                     placeholder="james.cook@gmail.com"
                     required
                 />
-                <label htmlFor="password" className="login-section__label">
+                <label
+                    htmlFor="password"
+                    className="login-register-section__label"
+                >
                     Hasło
                 </label>
 
@@ -50,11 +56,14 @@ class LoginForm extends React.Component {
                     onChange={this.handlePasswordChange}
                     value={this.state.password}
                     id="password"
-                    className="login-section__input"
+                    className="login-register-section__input"
                     type="password"
                     required
                 />
-                <button type="submit" className="login-section__button">
+                <button
+                    type="submit"
+                    className="login-register-section__button"
+                >
                     Zaloguj się
                 </button>
                 <NoAccountInformation />
--- src/components/NoAccountInformation.jsx
+++ src/components/NoAccountInformation.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NoAccountInformation = () => (
-    <div className="login-section__no-account">
-        <small className="login-section__no-account-description">
+    <div className="login-register-section__no-account">
+        <small className="login-register-section__no-account-description">
             Nie masz konta w serwisie Series Advisor?
         </small>
-        <Link to="register" className="login-section__registration-link">
+        <Link to="register" className="login-register-section__registration-link">
             Zarejestruj się
         </Link>
     </div>
